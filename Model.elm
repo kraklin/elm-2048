f3e3c458228499c19a62c80fcf011b878ea9854b
Tomas Latal <latal.tomas@gmail.com>
2016-08-21 12:45:21 +0200

    Small fixes to view

--- Model.elm
+++ Model.elm
@@ -1,4 +1,4 @@
-module Model exposing (Model, init)
+module Model exposing (Model, init, allTilesMatrix)
 
 import Matrix exposing (..)
 
@@ -9,10 +9,29 @@ type alias Model =
 
 
 init : ( Model, Cmd a )
-init =
+init = allTilesMatrix
+    -- ( { matrix =
+    --         Matrix.repeat 4 4 (Nothing)
+    --             |> Matrix.set 0 0 (Just 2)
+    --   }
+    -- , Cmd.none
+    -- )
+
+allTilesMatrix : ( Model, Cmd a )
+allTilesMatrix =
     ( { matrix =
             Matrix.repeat 4 4 (Nothing)
                 |> Matrix.set 0 0 (Just 2)
+                |> Matrix.set 0 1 (Just 4)
+                |> Matrix.set 0 2 (Just 8)
+                |> Matrix.set 0 3 (Just 16)
+                |> Matrix.set 1 0 (Just 32)
+                |> Matrix.set 1 1 (Just 64)
+                |> Matrix.set 1 2 (Just 128)
+                |> Matrix.set 1 3 (Just 256)
+                |> Matrix.set 2 0 (Just 512)
+                |> Matrix.set 2 1 (Just 1024)
+                |> Matrix.set 2 2 (Just 2048)
       }
     , Cmd.none
     )
--- View.elm
+++ View.elm
@@ -3,7 +3,7 @@ module View exposing (view)
 import Update exposing (Msg)
 import Model exposing (Model)
 import Html exposing (Html, div, text)
-import Html.Attributes
+import Html.Attributes exposing (style)
 import Matrix exposing (..)
 import Array exposing (..)
 
@@ -11,44 +11,45 @@ import Array exposing (..)
 -- View
 
 
-prettyPrint : Matrix (Maybe Int) -> Html.Html Msg
+baseTileStyle : List ( String, String )
+baseTileStyle =
+    [ ( "width", "100px" )
+    , ( "height", "100px" )
+    , ( "border-radius", "10px" )
+    , ( "margin", "5px" )
+    , ( "display", "inline-block" )
+    , ( "line-height", "100px" )
+    , ( "font-size", "60px" )
+    , ( "font-weight", "bold" )
+    , ( "text-align", "center" )
+    ]
+
+
+prettyPrint : Matrix (Maybe Int) -> Html Msg
 prettyPrint matrix =
     matrix
         |> Matrix.indexedMap drawTile
         |> matrixToDivs
 
 
-drawTile : Int -> Int -> Maybe Int -> Html.Html Msg
+drawTile : Int -> Int -> Maybe Int -> Html Msg
 drawTile x y value =
     case value of
         Nothing ->
-            Html.div
-                [ Html.Attributes.style
-                    [ ( "width", "40px" )
-                    , ( "height", "40px" )
-                    , ( "border-radius", "4px" )
-                    , ( "margin", "2px" )
-                    , ( "display", "inline-block" )
-                    , ( "line-height", "40px" )
-                    , ( "font-size", "12px" )
-                    , ( "background-color", "lightgrey" )
-                    ]
+            div
+                [ style
+                    (baseTileStyle
+                        ++ [ ( "background-color", "lightgray" ) ]
+                    )
                 ]
-                []
+                [ text " " ]
 
         Just value ->
-            Html.div
-                [ Html.Attributes.style
-                    [ ( "width", "40px" )
-                    , ( "height", "40px" )
-                    , ( "border-radius", "4px" )
-                    , ( "margin", "2px" )
-                    , ( "display", "inline-block" )
-                    , ( "line-height", "40px" )
-                    , ( "font-size", "12px" )
-                    , ( "text-align", "center" )
-                    , ( "background-color", "green" )
-                    ]
+            div
+                [ style
+                    (baseTileStyle
+                        ++ [ ( "background-color", "yellow" ) ]
+                    )
                 ]
                 [ text (toString value) ]
 
@@ -72,6 +73,24 @@ matrixToDivs matrix =
 
 view : Model -> Html.Html Msg
 view model =
-    div []
-        [ prettyPrint model.matrix
+    div
+        [ style
+            [ ( "background", "#faf8ef" )
+            , ( "color", "#776e65" )
+            , ( "margin", "0 auto" )
+            ]
+        ]
+        [ div
+            [ style
+                [ ( "width", "500px" )
+                , ( "height", "100%" )
+                , ( "margin", "0 auto" )
+                , ( "font-family", "\"Clear Sans\", \"Helvetica Neue\", Arial, sans-serif;" )
+                , ( "font-size", "18px" )
+                ]
+            ]
+            [ div []
+                [ prettyPrint model.matrix
+                ]
+            ]
         ]
